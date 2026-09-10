89bc2b9726130af6b1ddb9846fc36ae52e4117a7
buithanhthuan0698-design <buithanhthuan0698@gmail.com>
2026-09-10 14:00:54 +0700

    Add product CRUD API

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const db = require("./db");
+const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 
@@ -13,17 +13,7 @@ app.get("/", (req, res) => {
   res.send("Ecommerce Backend API is running");
 });
 
-app.get("/api/products", async (req, res) => {
-  try {
-    const [rows] = await db.query("SELECT * FROM products");
-    res.json(rows);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      message: "Lỗi kết nối database",
-    });
-  }
-});
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5001;
 

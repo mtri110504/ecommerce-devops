e29d137207b12b4bd7e9f775ac5bffc83d5ffd85
buithanhthuan0698-design <buithanhthuan0698@gmail.com>
2026-09-18 15:38:58 +0700

    Provision AWS ECR repositories with Terraform

---
+++ terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "backend_ecr_url" {
+  description = "Backend ECR repository URL"
+  value       = aws_ecr_repository.backend.repository_url
+}
+
+output "frontend_ecr_url" {
+  description = "Frontend ECR repository URL"
+  value       = aws_ecr_repository.frontend.repository_url
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {
+  description = "AWS region used for CloudTech infrastructure"
+  type        = string
+  default     = "ap-southeast-1"
+}
